058cda1c20b745bea501da975809ef67b144bf59
Patrick Ceriale <patceriale@gmail.com>
2020-09-23 10:35:26 -0700

    trying to fix the world

--- JS/testingSearch.js
+++ JS/testingSearch.js
@@ -1,54 +1,26 @@
 //TODO:// function to search through ajax
-var city = $("#cityInput")
+var city = $("#cityInput");
 console.log("hello");
 testingSite();
 function testingSite() {
-    //The state input field 
-    var state = $(this).value;
-    //Ajax request
-    $.ajax({
-        url: "https://covid-19-testing.github.io/locations/" + "washington" + "/complete.json",
-        method: "GET"
-    }).then(function(response) {
-        //testing response
-        console.log(response);
-
-        //TODO://setup a loop to pull from each object based on city
-        for (var i = 0; i < response.length; i++) {
-                //Creating container for testing location
-                var location = $("<div>");
-                    location.addClass("pure-u-1-3")
-                //Location Name
-                var locationName = $("<h1>");
-                    location.text(response[i].name);
-                //Phone number
-                var phoneNumber = $("<p>");
-                    phoneNumber.text(response[i].phones[0].number);
-                //Address
-                var locationAddress = $("<p>");
-                    locationAddress.text(response[i].physical_address[0].address_1 + " " + response[i].physical_address[0].city + " " + response[i].physical_address[0].state_province);
-                //Hours or operations
-                var hoursOperation = $("<p>");
-                    hoursOperation.text()
-                var description = $("<p>");
-                    description.text(response[i].description);
-                //Appending to div
-                location.append(locationName);
-                location.append(phoneNumber);
-                location.append(locationAddress);
-                location.append(description);
-                //Appending to the page
-                $("#body").append(location);
-            
-        }
-    $("#state").text(" ");
-    })}
+  //The state input field
+  var state = $(this).value;
+  //Ajax request
+  $.ajax({
+    url:
+      "https://covid-19-testing.github.io/locations/" +
+      "washington" +
+      "/complete.json",
+    method: "GET",
+  }).then(function (response) {
+    //testing response
+    console.log(response);
 
     //TODO://setup a loop to pull from each object based on city
     for (var i = 0; i < response.length; i++) {
       //Creating container for testing location
       var location = $("<div>");
-      location.addClass("pure-g");
+      location.addClass("pure-u-1-3");
       //Location Name
       var locationName = $("<h1>");
       location.text(response[i].name);
@@ -77,9 +49,44 @@ function testingSite() {
       //Appending to the page
       $("#body").append(location);
     }
+    $("#state").text(" ");
   });
+}
+
+//TODO://setup a loop to pull from each object based on city
+for (var i = 0; i < response.length; i++) {
+  //Creating container for testing location
+  var location = $("<div>");
+  location.addClass("pure-g");
+  //Location Name
+  var locationName = $("<h1>");
+  location.text(response[i].name);
+  //Phone number
+  var phoneNumber = $("<p>");
+  phoneNumber.text(response[i].phones[0].number);
+  //Address
+  var locationAddress = $("<p>");
+  locationAddress.text(
+    response[i].physical_address[0].address_1 +
+      " " +
+      response[i].physical_address[0].city +
+      " " +
+      response[i].physical_address[0].state_province
+  );
+  //Hours or operations
+  var hoursOperation = $("<p>");
+  hoursOperation.text();
+  var description = $("<p>");
+  description.text(response[i].description);
+  //Appending to div
+  location.append(locationName);
+  location.append(phoneNumber);
+  location.append(locationAddress);
+  location.append(description);
+  //Appending to the page
+  $("#body").append(location);
 }
 
 //TODO: click event from a button
 
-$("#state").on("submit", testingSite)
+$("#state").on("submit", testingSite);
