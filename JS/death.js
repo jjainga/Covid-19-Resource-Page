322860a2ad2d3926b0c9927d93f8026fdbbde1f2
Darrian DeVaughnn <devondevaughnn@gmail.com>
2020-09-24 11:47:38 -0700

    added a border around the death toll. Can be changed but just wanted it to stick out

--- JS/death.js
+++ JS/death.js
@@ -10,8 +10,6 @@ function getDeaths(deaths) {
     //Setting variables to get the increase in deaths, national death count, those in ICU, and the current day
     var natDeathCount = deaths.deaths.value;
     console.log(natDeathCount)
-    
-    
     //This can be modified to take away currentDay if the date is too specific. 
     $("#deathToll").append("Our National death toll is " + natDeathCount + ". Use caution. Please wear a mask.");
     //Adds counts onto page
