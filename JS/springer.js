0bac0bb5409fa7322c895a9ee2d77c8856a05169
Patrick Ceriale <patceriale@gmail.com>
2020-09-24 10:08:45 -0700

    pulling dev over

--- JS/springer.js
+++ JS/springer.js
@@ -3,27 +3,35 @@ console.log("Hello world!");
 let nytApiKey = "D7y7qtTuA0tnCOzIi5pI5W9s14Fgb7QY";
 let searchTerm = "COVID-19"; // TODO: Test other key words to see if they are more relevant
 let nytQueryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${nytApiKey}&q=${searchTerm}`;
+let googleURL = `https://serpapi.com/search.json?engine=google&q=scholarly+articles+for+covid-19&google_domain=google.com&gl=us&hl=en&api_key=ca699a8bfbc1d55e7dd666c92f2e53d07cba60ad16625b48fc32e844109521e4`;
 
-//AJAX call for NYT Developer Network
 $.ajax({
-  url: nytQueryURL,
-  method: "GET",
-}).then(updatePage);
-
-//AJAX call for Springer Nature
-$.ajax({
-  url:
-    "http://api.springernature.com/meta/v2/json?q=keyword:clinical studies covid-19 onlinedatefrom:2020-01-01&api_key=bfbcaf96f0d13448d0bcf0757b9411c4",
+  url: googleURL,
   method: "GET",
 }).then(function (data) {
   console.log(data);
-  console.log(data.records[0].abstract);
+  console.log("_--_-___---____---_---__--_-_-_-");
 });
+
+// //AJAX call for NYT Developer Network
+// $.ajax({
+//   url: nytQueryURL,
+//   method: "GET",
+// }).then(updatePage);
+
+// //AJAX call for Springer Nature
+// $.ajax({
+//   url:
+//     "http://api.springernature.com/meta/v2/json?q=keyword:clinical studies covid-19 onlinedatefrom:2020-01-01&api_key=bfbcaf96f0d13448d0bcf0757b9411c4",
+//   method: "GET",
+// }).then(function (data) {
+//   console.log(data);
+//   console.log(data.records[0].title);
+// });
 
 // Calling the NYT articles into existence
 function updatePage(NYTData) {
   let numArticles = 5;
-  //let articleDoc = NYTData.response;
 
   //console.log(NYTData); //TODO: remove console.log()
   // Repeat as many times as articles desired on page
