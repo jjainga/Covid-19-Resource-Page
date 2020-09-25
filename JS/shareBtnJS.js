1d8a5f86ea0d39f71a16e2e779142af89f644108
Darrian DeVaughnn <devondevaughnn@gmail.com>
2020-09-25 12:37:45 -0700

    created new css/html/js for a share page. Right now it takes us to the website, but Im working on getting the actual share function a given article

---
+++ JS/shareBtnJS.js
@@ -0,0 +1,6 @@
+var shrBtn = document.querySelector(".share_btn");
+var tgglBtn = document.querySelector("#shareCons");
+
+shrBtn.addEventListener("click", function () {
+   tgglBtn.classList.toggle("active");
+})
